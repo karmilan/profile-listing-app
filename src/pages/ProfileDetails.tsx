380fe7866107d938aeb3274d1a1fca90d6c8233e
karmilan <delftkarmilan@gmail.com>
2025-03-22 08:40:18 +0530

    removed unused code

--- src/pages/ProfileDetails.tsx
+++ src/pages/ProfileDetails.tsx
@@ -3,7 +3,6 @@ import ViewProfileCard from "../components/ViewProfileCard";
 
 const ProfileDetails = () => {
   const location = useLocation();
-  const { client_id, client_name, client_profile_url } = location.state;
   console.log(location.state);
 
   return (
