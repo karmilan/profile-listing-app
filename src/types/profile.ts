33131041509bd5e1d193f66a960477e9c949ab8d
karmilan <delftkarmilan@gmail.com>
2025-03-22 08:12:24 +0530

    profile type updated

--- src/types/profile.ts
+++ src/types/profile.ts
@@ -2,6 +2,6 @@ export type Profile = {
   client_id: string;
   client_name: string;
   client_profile_url: string;
-  client_city: string;
-  client_mobile: number;
+  client_city?: string;
+  client_mobile?: number;
 };
